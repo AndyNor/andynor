d7e7c8842d40eb7aca653e39a36f178d43145de4
André Nordbø <andre.nordbo@gmail.com>
2020-04-18 16:46:05 +0200

    updated prod keys

--- secrets_prod.py
+++ secrets_prod.py
@@ -2,3 +2,6 @@
 import os
 def load_secrets():
 	os.environ["SECRET_KEY"] = 'e@xl!ge54tu_t6(d6&amp;7s4c4l7bu2u3r=#_&bmp;dn@c3ihibjkoy@t'
+	os.environ["SBANKEN_CUSTOMERID"] = '16118439353'
+	os.environ["SBANKEN_CLIENTID"] = '7b6e5a4d9f874011ad9b70414de75315'
+	os.environ["SBANKEN_SECRET"] = 'G84MGw=qlNcqifL3cE3OflqcD-RKIrZY+jGnePX1Muhu=e7pMmFSA51Ol1BROH6E'
