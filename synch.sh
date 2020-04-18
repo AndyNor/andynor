5631eb121c034cc343841812273719402dbe9853
André Nordbø <andre.nordbo@gmail.com>
2020-04-18 18:29:00 +0200

    a

--- synch.sh
+++ synch.sh
@@ -10,3 +10,7 @@ python3.7 manage.py migrate
 python3.7 manage.py collectstatic --noinput
 
 ~/webapps/django225/apache2/bin/restart
+
+chmod +x synch.sh
+chmod +x restart.sh
+chmod +x sbanken_api.sh
