2588594bccb07d1399a8bdbd566efb2024e21236
André Nordbø <andre.nordbo@gmail.com>
2020-12-10 22:22:47 +0100

    Delete secrets_prod.py

--- secrets_prod.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import os
-def load_secrets():
-	os.environ["SECRET_KEY"] = 'e@xl!ge54tu_t6(d6&amp;7s4c4l7bu2u3r=#_&bmp;dn@c3ihibjkoy@t'
-	os.environ["SBANKEN_CUSTOMERID"] = '16118439353'
-	os.environ["SBANKEN_CLIENTID"] = '7b6e5a4d9f874011ad9b70414de75315'
-	os.environ["SBANKEN_SECRET"] = 'tSLoUP=YDYmqra0ShbxEvtc=Eb0jdDjnjYfIDKFKrKzcr9Z1qAJU9qq?AN?vF94g'
-	#os.environ["SBANKEN_SECRET"] = 'G84MGw=qlNcqifL3cE3OflqcD-RKIrZY+jGnePX1Muhu=e7pMmFSA51Ol1BROH6E'
