d0d6dd8f8197ba787cd4dc12f83a6655d45d44cf
André Nordbø <andre.nordbo@gmail.com>
2021-03-03 21:25:41 +0100

    swagger

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,6 +6,10 @@ from django.views.generic.base import RedirectView
 from mysite import views
 from django.urls import path
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 handler403 = 'mysite.views.my_custom_permission_denied_view'
 admin.autodiscover()
 
@@ -35,6 +39,25 @@ urlpatterns += [
 	path('calc/', include('calc.urls'), name="calc"),
 	path('stocks/', include('stocks.urls'), name="stocks"),
 	path('conan/', include('conan.urls'), name="conan"),
+]
+
+schema_view = get_schema_view(
+	openapi.Info(
+		title="Conan API's",
+		default_version='v1',
+		description="",
+		terms_of_service="",
+		contact=openapi.Contact(email=""),
+		license=openapi.License(name=""),
+	),
+	public=True,
+	permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+	url(r'^conan/swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+	url(r'^conan/swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+	url(r'^conan/redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 
--- settings.py
+++ settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'rest_framework',
+    'drf_yasg',
     'mysite',
     'money',
     'power',
