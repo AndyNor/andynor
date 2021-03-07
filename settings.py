5275fa7a0882fc97b0e94a02bd68afdc572079f1
André Nordbø <andre.nordbo@gmail.com>
2021-03-07 14:52:43 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = (
 
 REST_FRAMEWORK = {
 	'DEFAULT_PERMISSION_CLASSES': (
-		'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
 	)
 }
 
