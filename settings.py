fc1cc1d335537ef01f8bf5d382cd3a11b8e5702e
André Nordbø <andre.nordbo@gmail.com>
2024-10-27 16:25:50 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,53 +7,55 @@ this_env()
 THIS_ENVIRONMENT = os.environ['THIS_ENV'] # "PROD" / "TEST" / "DEV"
 
 if THIS_ENVIRONMENT == "PROD":
-    SITE_URL = "https://andynor.net"
-    ALLOWED_HOSTS = ['.andynor.net']
-    DEBUG = False
-    from secrets_prod import load_secrets
-    load_secrets()
+	SITE_URL = "https://andynor.net"
+	ALLOWED_HOSTS = ['.andynor.net']
+	DEBUG = True
+	from secrets_prod import load_secrets
+	load_secrets()
+
 if THIS_ENVIRONMENT == "DO_PROD":
-    SITE_URL = "https://andynor.net"
-    ALLOWED_HOSTS = ['.andynor.net']#, '161.35.216.174']
-    DEBUG = False
-    from secrets_prod import load_secrets
-    load_secrets()
+	SITE_URL = "https://andynor.net"
+	ALLOWED_HOSTS = ['.andynor.net']#, '161.35.216.174']
+	DEBUG = False
+	from secrets_prod import load_secrets
+	load_secrets()
+
 if THIS_ENVIRONMENT == "DEV":
-    SITE_URL = "localhost:8001"
-    ALLOWED_HOSTS = ['localhost',]
-    DEBUG = True
-    from secrets_dev import load_secrets
-    load_secrets()
+	SITE_URL = "localhost:8001"
+	ALLOWED_HOSTS = ['localhost',]
+	DEBUG = True
+	from secrets_dev import load_secrets
+	load_secrets()
 
 WSGI_APPLICATION = 'wsgi.application'
 ROOT_URLCONF = 'mysite.urls'
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.humanize',
-    'rest_framework',
-    'drf_yasg',
-    'mysite',
-    'money',
-    'power',
-    'blog',
-    'databases',
-    'calc',
-    'stocks',
-    'conan',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'django.contrib.humanize',
+	'rest_framework',
+	'drf_yasg',
+	'mysite',
+	'money',
+	'power',
+	'blog',
+	'databases',
+	'calc',
+	'stocks',
+	'conan',
 )
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 REST_FRAMEWORK = {
 	'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-        #'rest_framework.authentication.BasicAuthentication',
+		'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+		#'rest_framework.authentication.BasicAuthentication',
 	),
 }
 
@@ -69,43 +71,42 @@ SESSION_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SAMESITE = "Strict"
 
 if THIS_ENVIRONMENT == "PROD":
-    CSRF_COOKIE_SECURE = True
-    SESSION_COOKIE_SECURE = True
-    SECURE_HSTS_SECONDS = 31536000
+	CSRF_COOKIE_SECURE = True
+	SESSION_COOKIE_SECURE = True
+
 if THIS_ENVIRONMENT == "DO_PROD":
-    CSRF_COOKIE_SECURE = True
-    SESSION_COOKIE_SECURE = False
-    SECURE_HSTS_SECONDS = 31536000
+	CSRF_COOKIE_SECURE = True
+	SESSION_COOKIE_SECURE = False
+
 if THIS_ENVIRONMENT == "DEV":
-    CSRF_COOKIE_SECURE = False
-    SESSION_COOKIE_SECURE = False
-    SECURE_HSTS_SECONDS = None
+	CSRF_COOKIE_SECURE = True
+	SESSION_COOKIE_SECURE = False
 
 LOGIN_URL = '/login/'
 
 if THIS_ENVIRONMENT == "PROD":
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': '/home/andynor/webapps/django225/myproject/db.sqlite3',
-        }
-    }
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.sqlite3',
+			'NAME': '/home/andynor/webapps/django225/myproject/db.sqlite3',
+		}
+	}
 
 if THIS_ENVIRONMENT == "DO_PROD":
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': '/home/django/django_project/andynor/db.sqlite3',
-        }
-    }
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.sqlite3',
+			'NAME': '/home/django/django_project/andynor/db.sqlite3',
+		}
+	}
 
 if THIS_ENVIRONMENT == "DEV":
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'db.sqlite3',
-        }
-    }
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.sqlite3',
+			'NAME': 'db.sqlite3',
+		}
+	}
 
 TIME_ZONE = 'Europe/Oslo'
 LANGUAGE_CODE = 'nb-NO'
@@ -117,53 +118,53 @@ USE_I18N = True
 
 
 if THIS_ENVIRONMENT == "PROD":
-    MEDIA_ROOT = '/home/andynor/webapps/static_media/'
-    MEDIA_URL = '/media/'
-    FILE_ROOT = '/home/andynor/webapps/static_media/fileuploads/'
-    FILE_URL = '/media/fileuploads/'
-    STATIC_ROOT = '/home/andynor/webapps/static/'
-    STATIC_URL = '/static/'
+	MEDIA_ROOT = '/home/andynor/webapps/static_media/'
+	MEDIA_URL = '/media/'
+	FILE_ROOT = '/home/andynor/webapps/static_media/fileuploads/'
+	FILE_URL = '/media/fileuploads/'
+	STATIC_ROOT = '/home/andynor/webapps/static/'
+	STATIC_URL = '/static/'
 
 if THIS_ENVIRONMENT == "DO_PROD":
-    MEDIA_ROOT = '/home/django/django_project/andynor/media/'
-    MEDIA_URL = '/media/'
-    FILE_ROOT = '/home/django/django_project/andynor/media/fileuploads/'
-    FILE_URL = '/media/fileuploads/'
-    STATIC_ROOT = '/home/django/django_project/andynor/static/'
-    STATIC_URL = '/static/'
+	MEDIA_ROOT = '/home/django/django_project/andynor/media/'
+	MEDIA_URL = '/media/'
+	FILE_ROOT = '/home/django/django_project/andynor/media/fileuploads/'
+	FILE_URL = '/media/fileuploads/'
+	STATIC_ROOT = '/home/django/django_project/andynor/static/'
+	STATIC_URL = '/static/'
 
 if THIS_ENVIRONMENT == "DEV":
-    MEDIA_ROOT = '_media/'
-    MEDIA_URL = '/media/'
-    FILE_ROOT = '_media/fileuploads/'
-    FILE_URL = '/media/fileuploads/'
-    STATIC_ROOT = '_static/'
-    STATIC_URL = '/static/'
+	MEDIA_ROOT = '_media/'
+	MEDIA_URL = '/media/'
+	FILE_ROOT = '_media/fileuploads/'
+	FILE_URL = '/media/fileuploads/'
+	STATIC_ROOT = '_static/'
+	STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = (
 )
 
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    #'django.contrib.staticfiles.finders.FileSystemFinder',
-    #'django.contrib.staticfiles.finders.DefaultStorageFinder',
+	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+	#'django.contrib.staticfiles.finders.FileSystemFinder',
+	#'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 MIDDLEWARE = (
-    'django.middleware.security.SecurityMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'mysite.middleware.HTTPSRedirect',
-    'mysite.middleware.CountVisitor',
-    'mysite.middleware.SessionCleanup',
-    'mysite.middleware.SecurityHeaders',
-    'csp.middleware.CSPMiddleware',
+	'django.middleware.security.SecurityMiddleware',
+	'django.middleware.locale.LocaleMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'mysite.middleware.HTTPSRedirect',
+	'mysite.middleware.CountVisitor',
+	'mysite.middleware.SessionCleanup',
+	'mysite.middleware.SecurityHeaders',
+	'csp.middleware.CSPMiddleware',
 )
 
 # Security headers
@@ -179,21 +180,21 @@ SECURE_CONTENT_TYPE_NOSNIFF = True  # requires "SecurityMiddleware"
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.media',
-                'django.template.context_processors.static',
-                'django.template.context_processors.tz',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.request',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.contrib.auth.context_processors.auth',
+				'django.template.context_processors.debug',
+				'django.template.context_processors.i18n',
+				'django.template.context_processors.media',
+				'django.template.context_processors.static',
+				'django.template.context_processors.tz',
+				'django.contrib.messages.context_processors.messages',
+				'django.template.context_processors.request',
+			],
+		},
+	},
 ]
