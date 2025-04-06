0395264c64b440a633ba660cad840e75bdbeeb71
André Nordbø <andre.nordbo@gmail.com>
2025-04-06 11:51:08 +0200

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ admin.autodiscover()
 
 # Mysite
 urlpatterns = [
-	re_path(r'^forside/$', views.index, name="root"),
+	re_path(r'^$', views.index, name="root"),
 	re_path(r'^login/$', views.user_login, name="user_login"),
 	re_path(r'^logout/$', views.user_logout, name="user_logout"),
 	re_path(r'^password/$', views.password_change, name="password_change"),
