5a268ad57c106f14034e86145aa122c4fc884afa
André Nordbø <andre.nordbo@gmail.com>
2026-04-23 23:17:07 +0200

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,76 @@
 from django.contrib import admin
+
 from blog import models
 
-admin.site.register(models.Category)
-admin.site.register(models.Group)
-admin.site.register(models.Tag)
-admin.site.register(models.Blog)
-admin.site.register(models.Comment)
-admin.site.register(models.Image)
-admin.site.register(models.File)
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('category', 'visible', 'tabbed', 'grouped')
+	list_filter = ('visible', 'tabbed', 'grouped')
+	search_fields = ('category', 'description')
+
+
+@admin.register(models.Group)
+class GroupAdmin(admin.ModelAdmin):
+	list_display = ('name',)
+	search_fields = ('name',)
+
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+	list_display = ('tag', 'description')
+	search_fields = ('tag', 'description')
+
+
+@admin.register(models.Blog)
+class BlogAdmin(admin.ModelAdmin):
+	list_display = (
+		'id',
+		'title',
+		'category',
+		'origin',
+		'published',
+		'sticky',
+		'linked',
+		'owner',
+		'updated',
+	)
+	list_filter = ('published', 'sticky', 'linked', 'category', 'group')
+	search_fields = ('title', 'content', 'tags__tag')
+	date_hierarchy = 'origin'
+	list_select_related = ('category', 'owner', 'group')
+	ordering = ('-updated',)
+
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ('id', 'page', 'poster', 'comment_preview', 'created', 'ip')
+	list_select_related = ('page',)
+	search_fields = ('comment', 'poster', 'page__title')
+	list_filter = ('created',)
+
+	@admin.display(description='Comment')
+	def comment_preview(self, obj):
+		text = (obj.comment or '')[:80]
+		return text + ('…' if len(obj.comment or '') > 80 else '')
+
+
+@admin.register(models.Image)
+class ImageAdmin(admin.ModelAdmin):
+	list_display = ('id', 'filename', 'blog', 'order', 'description_preview')
+	list_select_related = ('blog', 'owner')
+	search_fields = ('filename', 'description', 'blog__title')
+	list_filter = ('blog',)
+
+	@admin.display(description='Description')
+	def description_preview(self, obj):
+		text = (obj.description or '')[:60]
+		return text + ('…' if len(obj.description or '') > 60 else '')
+
+
+@admin.register(models.File)
+class FileAdmin(admin.ModelAdmin):
+	list_display = ('id', 'filename', 'blog', 'size', 'created', 'owner')
+	list_select_related = ('blog', 'owner')
+	search_fields = ('filename', 'checksum', 'blog__title')
+	list_filter = ('created',)
